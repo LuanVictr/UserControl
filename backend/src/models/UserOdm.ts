76685b5d0aaf74713a24407015adb96ef161e354
LuanVictr <luanvictor2108@gmail.com>
2023-10-24 15:44:42 -0300

    Feat: Implementando Odm de usuarios (UserOdm)

---
+++ backend/src/models/UserOdm.ts
@@ -0,0 +1,22 @@
+import { Model, Schema, model, models } from 'mongoose';
+import IUser from '../interfaces/IUser';
+
+class UserOdm {
+
+  private model: Model<IUser>;
+
+  private modelName:string = 'Users';
+  
+  constructor() {
+    const schema = new Schema<IUser>({
+      email: { type: String, required: true },
+      password: { type: String, required: true },
+      role: { type: String, required: true },
+    }, { versionKey: false });
+
+    this.model = models[this.modelName] || model(this.modelName, schema);
+
+  }
+}
+
+export default UserOdm;
